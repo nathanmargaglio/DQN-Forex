3d38b670850d149b4ac080988815a1663f52af74
Nathan <nathanmargaglio@gmail.com>
2018-09-24 09:16:17 -0400

    quick update to gif script

--- frames_to_gif.sh
+++ frames_to_gif.sh
@@ -1,3 +1,4 @@
+cd dqn/frames
 rename 's/\d+/sprintf("%05d",$&)/e' *.png
 ffmpeg -i %05d.png -vf palettegen palette.png
 ffmpeg -framerate 60 -i %05d.png -i palette.png -lavfi "paletteuse,setpts=6*PTS" -y play.gif
