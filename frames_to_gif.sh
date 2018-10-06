947657e534de1d6714ddc22d25f8f1db1bbf2f6c
Nathan <nathanmargaglio@gmail.com>
2018-10-06 12:16:06 -0400

    testing training v16

--- frames_to_gif.sh
+++ frames_to_gif.sh
@@ -2,4 +2,6 @@ cd dqn/frames
 #rename 's/\d+/sprintf("%05d",$&)/e' *.png
 ffmpeg -i %05d.png -vf palettegen palette.png -y
 ffmpeg -framerate 60 -i %05d.png -i palette.png -lavfi "paletteuse,setpts=6*PTS" -y play.gif
+ffmpeg -framerate 120 -i %05d.png -i palette.png -lavfi "paletteuse,setpts=6*PTS" -y playx2.gif
 mv play.gif ../play.gif
+mv playx2.gif ../playx2.gif
